b8221189ded145392543603d5e6b88dae5d7921d
Jett <jettburns11@icloud.com>
2017-12-23 18:40:58 -0600

    Create Points leaderboard (Non-hackers).sql

---
+++ Originals/Points leaderboard (Non-hackers).sql
@@ -0,0 +1,34 @@
+// Link:  https://www.khanacademy.org/computer-programming/points-leaderboard-non-hackers/6085401675169792
+// Created:  10/28/2015 08:21
+
+
+
+
+CREATE TABLE users (id INTEGER PRIMARY KEY, points INTEGER, name TEXT);
+
+/* https://www.khanacademy.org/profile/Carlof101stAirborneDivisionOperationOverlord/discussion */
+
+INSERT INTO users VALUES (1, "22,244,547", "https://www.khanacademy.org/profile/katisusanna/");
+INSERT INTO users VALUES (2, "16,821,097", "https://www.khanacademy.org/profile/beast314/");
+INSERT INTO users VALUES (3, "13,484,895", "https://www.khanacademy.org/profile/Sphairistrike/");
+INSERT INTO users VALUES (4, "10,935,501", "https://www.khanacademy.org/profile/kingkoek/");
+INSERT INTO users VALUES (5, "10,827,113", "https://www.khanacademy.org/profile/Graham/");
+INSERT INTO users VALUES (6, "10,798,393", "https://www.khanacademy.org/profile/Rocky/");
+INSERT INTO users VALUES (7, "10,607,050", "https://www.khanacademy.org/profile/benjw/");
+INSERT INTO users VALUES (8, "10,375,936", "https://www.khanacademy.org/profile/Maestro1/");
+INSERT INTO users VALUES (9, "10,305,460", "https://www.khanacademy.org/profile/tpt123/");
+INSERT INTO users VALUES (10, "10,299,670", "https://www.khanacademy.org/profile/BRADSIEMENS/");
+INSERT INTO users VALUES (11, "10,194,711", "https://www.khanacademy.org/profile/Madeliv/");
+INSERT INTO users VALUES (12, "9,466,900", "https://www.khanacademy.org/profile/Dayvyd/");
+INSERT INTO users VALUES (13, "8,928,756", "https://www.khanacademy.org/profile/stanley.liu/");
+INSERT INTO users VALUES (14, "8,708,790", "https://www.khanacademy.org/profile/trekcelt/");
+INSERT INTO users VALUES (15, "8,654,344", "https://www.khanacademy.org/profile/StephanBlad/");
+INSERT INTO users VALUES (16, "7,765,300", "https://www.khanacademy.org/profile/Pethree/");
+INSERT INTO users VALUES (17, "7,012,090", "Tywysog y Goron Carl O Gymru");
+INSERT INTO users VALUES (18, "6,066,770", "https://www.khanacademy.org/profile/vianmiranda23/");
+INSERT INTO users VALUES (19, "5,869,534", "https://www.khanacademy.org/profile/doctorfox/");
+INSERT INTO users VALUES (20, "5,774,866", "https://www.khanacademy.org/profile/ElpisGames/");
+INSERT INTO users VALUES (21, "5,149,696","https://www.khanacademy.org/profile/Scottyboy1/");
+INSERT INTO users VALUES (22, "4,967,775", "https://www.khanacademy.org/profile/InHo/");
+INSERT INTO users VALUES (23, "4,457,099", "https://www.khanacademy.org/profile/bsouthworth23/");
+SELECT * FROM users;
