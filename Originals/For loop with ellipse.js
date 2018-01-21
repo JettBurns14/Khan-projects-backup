210169bf85b939b66a26bd2c378f8f599e17dc5d
Jett <jettburns11@icloud.com>
2018-01-20 21:39:03 -0600

    Create For loop with ellipse.js

---
+++ Originals/For loop with ellipse.js
@@ -0,0 +1,13 @@
+// Link:  https://www.khanacademy.org/computer-programming/for-loop-with-ellipse/5766202504380416
+// Created:  01/08/2016 12:08
+
+
+
+
+
+// This could be used as a cool background for a game.
+noStroke();
+for (var i = 0; i < 117; i++) {
+    fill(30, 24, 199, 4);
+    ellipse(200, 200,(i*i)/15,(i*i)/15);
+}
